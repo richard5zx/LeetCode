ac464d334dc80ce2ecff403115f67c24aa062e86
Richard Chao <richardchao2009@gmail.com>
2022-05-30 09:28:43 +1000

    add more leet code stuff

---
+++ LC0027RemoveElement/LC0027RemoveElement.java
@@ -0,0 +1,20 @@
+public class LC0020ValidParantheses {
+    
+}
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        // Empty array
+        if (nums.length == 0) return 0;
+        
+        int valid_size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[valid_size] = nums[i];
+                valid_size++;
+            }
+        }
+        return valid_size;
+    }
+}
